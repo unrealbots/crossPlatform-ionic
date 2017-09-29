c5a47cdc2b2c9b6609684bce7104d9df074d0ce2
MA. Ghani <malik@mirai-apps.com>
2017-09-30 02:08:14 +0700

    Generated tampilan page

---
+++ src/pages/tampilan/tampilan.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { IonicPageModule } from 'ionic-angular';
+import { TampilanPage } from './tampilan';
+
+@NgModule({
+  declarations: [
+    TampilanPage,
+  ],
+  imports: [
+    IonicPageModule.forChild(TampilanPage),
+  ],
+})
+export class TampilanPageModule {}
