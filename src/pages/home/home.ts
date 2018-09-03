4f59a104dea520beed8bf51fdd7b03d80b6d66f2
MA. Ghani <malik@mirai-apps.com>
2018-09-03 23:07:22 +0700

    Initial commit week 2

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,27 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { FirstPage } from "../first/first";
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
-
+  firstPage = FirstPage;
+  params = {
+    nama: "Malik Abdul Ghani",
+    umur: 21
+  }
   constructor(public navCtrl: NavController) {
+  
+  }
 
+  btnFirstClicked(){
+    console.log("Button is clicked");
+  }
+
+  btnGoToFirstPage(){
+    this.navCtrl.push(FirstPage);
   }
 
 }
